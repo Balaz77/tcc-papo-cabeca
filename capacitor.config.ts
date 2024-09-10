feb0db18fcf389565ae424ae2086faf88df4cde2
Balaz77 <165727624+Balaz77@users.noreply.github.com>
2024-09-09 22:23:45 -0300

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'tcc.papo.cabeca',
+  appName: 'tcc-papo-cabeca',
+  webDir: 'www'
+};
+
+export default config;
